388caf2372226cd48f3cd4ea14147aea72939a55
adebayo-glitch <pillarbasetech@gmail.com>
2024-09-05 18:08:40 -0400

    Add get student courses

--- routes/studentCourseRouter.js
+++ routes/studentCourseRouter.js
@@ -7,6 +7,11 @@ router.get('/', (request, response) => {
     response.render('index', { title: 'Student Portal | Home' });
 })
 
+// GET student courses
+router.get('/courses', (request, response) => {
+    response. Render('courses', { title: 'Student Portal | Course Registration', students, courses });
+})
+
 
   
   module.exports = router;
