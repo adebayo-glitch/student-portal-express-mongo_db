13774961ecf50fb73a143e2341af1cf3fe3759fc
adebayo-glitch <pillarbasetech@gmail.com>
2024-09-15 19:07:09 -0400

    update the hostel view tempalte and the routes

--- routes/hostelRoutes.js
+++ routes/hostelRoutes.js
@@ -5,8 +5,8 @@ const hostelController = require('../controllers/hostelController');
 router.get('/register-hostels', hostelController.getHostels);
 router.post('/register-hostels', hostelController.addHostel);
 router.get('/hostels', hostelController.getStudentHostel);
-router.get('/:id/edit', hostelController.editStudentHostel);
-router.put('/:id', hostelController.updateStudentHostel);
-router.delete('/:id', hostelController.deleteStudentHostel);
+router.get('/hostels/:id/edit', hostelController.editStudentHostel);
+router.put('/hostels/:id', hostelController.updateStudentHostel);
+router.delete('/hostels/:id', hostelController.deleteStudentHostel);
 
 module.exports = router;
