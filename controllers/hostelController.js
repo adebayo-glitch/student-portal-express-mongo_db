aff8767fd268764c6fe4d195380545e91c5c2a6a
adebayo-glitch <pillarbasetech@gmail.com>
2024-09-15 18:56:52 -0400

    update the index and stduencontroller

--- controllers/hostelController.js
+++ controllers/hostelController.js
@@ -29,38 +29,41 @@ exports.getStudentHostel = async (req, res) => {
 
 // Get student hostel edit page
 exports.editStudentHostel = async (req, res) => {
-    try {
+  try {
       const hostel = await Hostel.findById(req.params.id);
-      console.logo(hostel)
-      if (!hostel) return res.status(404).send('hostel not found');
-      res.render('hostel-edit', { hostel });
-    } catch (error) {
+      if (!hostel) return res.status(404).send('Hostel not found');
+      res.render('hostel-edit', { title: 'Edit Hostel', hostel });
+  } catch (error) {
       res.status(500).send(error.message);
-    }
-  };
-  
-  // Update student hostel 
-  exports.updateStudentHostel = async (req, res) => {
-    try {
+  }
+};
+
+// Update student hostel 
+exports.updateStudentHostel = async (req, res) => {
+  try {
       const { firstName, lastName, studentId, name, roomNumber } = req.body;
       const hostel = await Hostel.findByIdAndUpdate(req.params.id, {
-        firstName,
-        lastName,
-        studentId,
-        name,
-        roomNumber,
-        
+          firstName,
+          lastName,
+          studentId,
+          name,
+          roomNumber,
       }, { new: true });
-      if (!hostel) return res.status(404).send('hostel not found');
+      if (!hostel) return res.status(404).send('Hostel not found');
       res.redirect('/hostels');
-    } catch (error) {
+  } catch (error) {
       res.status(400).send(error.message);
-    }
-  };
+  }
+};
 
-// DELETE student Hostel
+// DELETE student hostel
 exports.deleteStudentHostel = async (req, res) => {
-    const { id } = req.params;
-    await Hostel.findByIdAndDelete(id);
-    res.status(200).send('Student hostel deleted successfully');
+  try {
+      const { id } = req.params;
+      const deletedHostel = await Hostel.findByIdAndDelete(id);
+      if (!deletedHostel) return res.status(404).send('Hostel not found');
+      res.redirect('/hostels'); 
+  } catch (error) {
+      res.status(500).send('Failed to delete hostel');
+  }
 };
