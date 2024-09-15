a20b0e8906f5772efeb7b3f89f2d43a913321517
adebayo-glitch <pillarbasetech@gmail.com>
2024-09-15 14:27:43 -0400

    create student course and get the course created

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -9,3 +9,33 @@ exports.getStudents = async (req, res) => {
      const courses = await Course.find();
     res.render('register-courses', { title: 'Student Portal | Course Registration', students, courses });
 };
+
+
+// POST create student with course registration
+exports.registerStudent = async (req, res) => {
+    const { firstName, lastName, studentId, department, session, courses } = req.body;
+    console.log(firstName)
+    console.log(department)
+    const newStudent = new Student({
+        firstName,
+        lastName,
+        studentId,
+        department,
+        session,
+        courses,
+        
+    });
+
+    await newStudent.save();
+    res.redirect('/courses');
+};
+
+// Get student courses
+exports.getStudentCourses = async (req, res) => {
+    const students = await Student.find().populate('courses', 'name');
+    // Fetch all available courses from the Course collection
+     const courses = await Course.find();
+    res.render('courses', { title: 'Student Portal | Course Registration', students, courses });
+};
+
+
